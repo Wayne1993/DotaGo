5078bb7bc2a613e97cebbc9fc5afd1d7d4f5f4f7
gongwei <gongwei1993625@163.com>
2017-12-13 17:42:03 +0800

    增加mybatis整合。

--- DotaGoService/src/main/java/com/fish/dotago/Application.java
+++ DotaGoService/src/main/java/com/fish/dotago/Application.java
@@ -1,5 +1,6 @@
 package com.fish.dotago;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 项目入口
  */
 @SpringBootApplication
+@MapperScan("com.fish.dotago.mapper")
 public class Application {
 
 	public static void main(String[] args) {
