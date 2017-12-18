2fd900dc879317025a96fc3507431510aa37bd47
sunzeyu <sunzeyu@zuihuibao.com>
2017-12-18 18:18:19 +0800

    修改MatchAPIController请求路径
    Signed-off-by: sunzeyu <sunzeyu@zuihuibao.com>

--- DotaGoService/src/main/java/com/fish/dotago/controller/MatchAPIController.java
+++ DotaGoService/src/main/java/com/fish/dotago/controller/MatchAPIController.java
@@ -1,39 +1,40 @@
 package com.fish.dotago.controller;
 
-import javax.annotation.Resource;
-
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fish.dotago.client.match.rep.GetMatchDetailRep;
 import com.fish.dotago.client.match.rep.GetMatchHistoryRep;
 import com.fish.dotago.client.match.req.GetMatchDetailReq;
 import com.fish.dotago.client.match.req.GetMatchHistoryReq;
 import com.fish.dotago.service.MatchAPIService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author cyy
  * @date 2017年12月14日
- * 
+ * <p>
  * match's controller
- *
  */
 @RestController
+@RequestMapping("/match")
 public class MatchAPIController {
 
-	@Resource
-	private MatchAPIService steamAPIService;
-	
-	@RequestMapping("/getMatchHistoryByAccountId")
-	public @ResponseBody GetMatchHistoryRep getMatchHistoryByAccountId(@RequestBody GetMatchHistoryReq getMatchHistoryReq) {
-		return steamAPIService.getMatchHistoryByAccountId(getMatchHistoryReq);
-	}
-	
-	@RequestMapping("/getMatchDetailByAccountId")
-	public @ResponseBody GetMatchDetailRep getMatchDetailByAccountId(@RequestBody GetMatchDetailReq getMatchDetailReq) {
-		return steamAPIService.getMatchDetailByMatchId(getMatchDetailReq);
-	}
-	
+    @Resource
+    private MatchAPIService steamAPIService;
+
+    @RequestMapping("/getMatchHistoryByAccountId")
+    public @ResponseBody
+    GetMatchHistoryRep getMatchHistoryByAccountId(@RequestBody GetMatchHistoryReq getMatchHistoryReq) {
+        return steamAPIService.getMatchHistoryByAccountId(getMatchHistoryReq);
+    }
+
+    @RequestMapping("/getMatchDetailByAccountId")
+    public @ResponseBody
+    GetMatchDetailRep getMatchDetailByAccountId(@RequestBody GetMatchDetailReq getMatchDetailReq) {
+        return steamAPIService.getMatchDetailByMatchId(getMatchDetailReq);
+    }
+
 }
